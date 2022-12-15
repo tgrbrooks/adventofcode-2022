2597cf6318b494fc0b3d72c17e235493704c14ad
tgrbrooks <thomas.brooks@appliedblockchain.com>
2022-12-15 11:09:10 +0000

    Solution 15.1

--- day15-1/solution.py
+++ day15-1/solution.py
@@ -1,41 +1,63 @@
 def parse_input(input):
   sensors = []
-  beacons = []
+  beacons = set()
   for line in input.split('\n'):
     sx = int(line.split('x=')[1].split(',')[0])
     sy = int(line.split('y=')[1].split(':')[0])
     sensors.append((sx, sy))
     bx = int(line.split('x=')[2].split(',')[0])
     by = int(line.split('y=')[2])
-    beacons.append((bx, by))
+    beacons.add((bx, by))
   return sensors, beacons
 
 def dist(s, b):
   return abs(s[0]-b[0]) + abs(s[1]-b[1])
 
+def overlaps(a, b):
+  if b[0] >= a[0] and b[0] <= a[1]:
+    return True
+  else:
+    return False
+
+def merge(arr):
+  merged_list= []
+  merged_list.append(arr[0])
+  for i in range(1, len(arr)):
+    pop_element = merged_list.pop()
+    if overlaps(pop_element, arr[i]):
+      new_element = pop_element[0], max(pop_element[1], arr[i][1])
+      merged_list.append(new_element)
+    else:
+      merged_list.append(pop_element)
+      merged_list.append(arr[i])
+  return merged_list
+
 def count_excluded(input, row):
   sensors, beacons = parse_input(input)
   closest = []
   for s in sensors:
-    cdist = dist(s, beacons[0])
-    for b in beacons[1:]:
-      if dist(s, b) < cdist:
+    cdist = None
+    for b in beacons:
+      if cdist is None or dist(s, b) < cdist:
         cdist = dist(s, b)
     closest.append(cdist)
-  excluded = set()
+  excluded = []
   for i, s in enumerate(sensors):
     drow = closest[i] - dist(s, (s[0], row))
     if drow < 0:
       continue
-    # Loop from closest[i] in row to (s[0], row)
-    for x in range(-drow, drow+1):
-      e = (s[0]+x, row)
-      if e not in beacons:
-        excluded.add(e)
+    excluded.append([s[0]-drow, s[0]+drow])
+  sorted_intervals = sorted(excluded)
+  print(sorted_intervals)
+  merged = merge(sorted_intervals)
+  print(merged)
   count = 0
-  for e in excluded:
-    if e[1] == row:
-      count += 1
+  for e in merged:
+    count += e[1] - e[0] + 1
+  print(count)
+  for b in beacons:
+    if b[1] == row:
+      count -= 1
   return count
 
 input = '''Sensor at x=391282, y=2038170: closest beacon is at x=-532461, y=2166525
@@ -73,4 +95,4 @@ Sensor at x=3917917, y=3792648: closest beacon is at x=3924443, y=3039680
 Sensor at x=40698, y=3202257: closest beacon is at x=227814, y=3107489
 Sensor at x=2619948, y=2439745: closest beacon is at x=2537630, y=2793941'''
 
-print(count_excluded(input, 10))
+print(count_excluded(input, 2000000))
